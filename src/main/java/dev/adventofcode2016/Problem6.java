7dd394b313c7a1f9a07621b9a9fcf9b0dd8e9f0f
John Hungerford <jhungerford@gmail.com>
2016-12-06 08:22:32 -0700

    Make problem 6 more readable.

--- src/main/java/dev/adventofcode2016/Problem6.java
+++ src/main/java/dev/adventofcode2016/Problem6.java
@@ -13,35 +13,51 @@ import java.util.stream.IntStream;
 
 public class Problem6 {
 
+  /** Sorts a map of character -> occurrences so the least common letter is first. */
   public static final Comparator<Map.Entry<Character, Integer>> LEAST_COMMON_LETTER =
       Map.Entry.comparingByValue();
 
+  /** Sorts a map of character -> occurrences so the most common letter is first. */
   public static final Comparator<Map.Entry<Character, Integer>> MOST_COMMON_LETTER =
       Map.Entry.<Character, Integer>comparingByValue().reversed();
 
+  /**
+   * Decodes a message from a list of lines.  The message is encoded using the frequency
+   * of letters in each column of the lines.
+   *
+   * @param lines List of lines containing characters.
+   * @param order Order of frequency to use to decode the message.
+   *              Use {@code LEAST_COMMON_LETTER} or {@code MOST_COMMON_LETTER}.
+   * @return Decoded message.
+   */
   public static String errorCorrected(List<String> lines, Comparator<Map.Entry<Character, Integer>> order) {
     int numColumns = lines.get(0).length();
 
-    // Compute a map of character -> occurrences for each column
-    List<Map<Character, Integer>> columnCount = IntStream.range(0, numColumns)
+    // Map of character -> occurrences for each column
+    List<Map<Character, Integer>> columnFrequencyMaps = IntStream.range(0, numColumns)
         .mapToObj(i -> new HashMap<Character, Integer>())
         .collect(Collectors.toList());
 
+    // Compute the frequency of each character in each column
     for (String line : lines) {
       for (int column = 0; column < numColumns; column ++) {
-        char c = line.charAt(column);
+        char character = line.charAt(column);
 
-        columnCount.get(column).compute(c, (key, count) -> count == null ? 1 : count + 1);
+        Map<Character, Integer> frequencyMap = columnFrequencyMaps.get(column);
+        frequencyMap.compute(character, (key, count) -> count == null ? 1 : count + 1);
       }
     }
 
-    // Take the most common character in each column to form the answer
-    return columnCount.stream()
+    // Take the most or least common character in each column to form the answer, determined by order
+    return columnFrequencyMaps.stream()
         .flatMap(map -> map.entrySet().stream()
+            // Most or least common character in the column
             .sorted(order)
-            .map(Map.Entry::getKey) // Most common character in the column
+            .map(Map.Entry::getKey)
             .limit(1)
-        ).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append) // Convert to string
+        )
+        // Convert to string
+        .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
         .toString();
   }
 
