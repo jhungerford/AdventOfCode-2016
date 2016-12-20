5eeed69c1b7512dbffaf43c94f0a9ed57d6e4f3a
John Hungerford <jhungerford@gmail.com>
2016-12-19 19:56:47 -0700

    Problem 19 part 2.

--- src/main/java/dev/adventofcode2016/Problem19.java
+++ src/main/java/dev/adventofcode2016/Problem19.java
@@ -13,7 +13,7 @@ public class Problem19 {
    * @param num Number of elves participating
    * @return Number of the elf that ends up with presents
    */
-  public static int elfWithPresents(int num) {
+  public static int elfWithPresents(int num, StealFromElf stealFromElf) {
     int numLeft = num;
 
     boolean[] playing = new boolean[num];
@@ -23,12 +23,12 @@ public class Problem19 {
 
     int turn = 0;
     while (numLeft > 1) {
-      int from = findRightElf(playing, turn);
+      int from = stealFromElf.from(playing, turn, numLeft);
 
       playing[from] = false;
       numLeft --;
 
-      turn = findRightElf(playing, turn);
+      turn = findRightElf(playing, turn, numLeft);
     }
 
     for (int i = 0; i < playing.length; i ++) {
@@ -48,7 +48,11 @@ public class Problem19 {
    * @param elf Elf who is taking a turn
    * @return Playing elf to the right of the given elf.
    */
-  public static int findRightElf(boolean[] playing, int elf) {
+  public static int findRightElf(boolean[] playing, int elf, int numLeft) {
+    if (numLeft == 1) {
+      return elf;
+    }
+
     for (int i = 1; i < playing.length; i ++) {
       int index = (elf + i) % playing.length;
       if (playing[index]) {
@@ -59,7 +63,81 @@ public class Problem19 {
     return elf;
   }
 
+  /**
+   * Picks the elf that is directly across the circle from the given elf.
+   *
+   * @param playing Elves who are playing
+   * @param elf Elf who is stealing presents
+   * @return Elf who presents will be stolen from, or the given elf if only one elf is playing
+   */
+  public static int acrossCircleElf(boolean[] playing, int elf, int numLeft) {
+    if (numLeft == 1) {
+      return elf;
+    }
+
+    int numToSkip = numLeft / 2; // Integer math rounds fractions down.
+    int numFound = 0;
+
+    for (int i = 1; i < playing.length; i ++) {
+      int index = (elf + i) % playing.length;
+
+      if (playing[index]) {
+        numFound ++;
+      }
+
+      if (numFound == numToSkip) {
+        return index;
+      }
+    }
+
+    return elf;
+  }
+
+  /**
+   * FunctionalInterface that determines which elf should be stolen from.
+   */
+  @FunctionalInterface
+  public interface StealFromElf {
+    /**
+     * Returns the elf that presents should be stolen from.
+     *
+     * @param playing Booleans indicating which elves are left in the game
+     * @param elf     Index of the elf that is stealing presents
+     * @param numLeft Number of elves left in the game
+     * @return Elf who presents will be stolen from, or the given elf if only one elf remains
+     */
+    int from(boolean[] playing, int elf, int numLeft);
+  }
+
+  public static int stealAcrossRemaining(int num) {
+    // Elf equation (determined by trials)
+    // Sequence resets to 1 after 3^n.  Sequence increases monotonically up to 3^n-1, then by twos up to 3^n.
+
+    // F(n) = n - 3^a              if n-3^a <= 3^a
+    //        3^a + 2*(n - 2*3^a)  otherwise
+
+    // Find the biggest power of three that's <= num.  (3^a <= num, but 3^(a+1) > num)
+    int a = 0;
+    while (Math.pow(3, a+1) < num) {
+      a++;
+    }
+
+    int threeA = (int) Math.pow(3, a); // 3^a
+
+    if (num - threeA <= threeA) {
+      return num - threeA; // n - 3^a
+    }
+
+    return threeA + 2 * (num - 2 * threeA); // 3^a + 2*(n - 2*3^a)
+  }
+
   public static void main(String[] args) {
-    System.out.println("Part 1: Elf " + Problem19.elfWithPresents(3014603) + " ends up with the presents");
+    int numElves = 3014603;
+
+    int part1 = Problem19.elfWithPresents(numElves, Problem19::findRightElf);
+    System.out.println("Part 1: Elf " + part1 + " ends up with the presents");
+
+    int part2 = Problem19.stealAcrossRemaining(numElves);
+    System.out.println("Part 2: Elf " + part2 + " ends up with the presents");
   }
 }
--- src/test/java/dev/adventofcode2016/Problem19Test.java
+++ src/test/java/dev/adventofcode2016/Problem19Test.java
@@ -7,15 +7,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Problem19Test {
 
   @Test
-  public void fiveElves() {
-    assertThat(Problem19.elfWithPresents(5)).isEqualTo(3);
+  public void fiveElvesStealToTheRight() {
+    assertThat(Problem19.elfWithPresents(5, Problem19::findRightElf)).isEqualTo(3);
   }
 
   @Test
   public void rightElf() {
-    assertThat(Problem19.findRightElf(new boolean[] {true, true, true, true, true}, 0)).isEqualTo(1);
-    assertThat(Problem19.findRightElf(new boolean[] {true, false, true, true, true}, 2)).isEqualTo(3);
-    assertThat(Problem19.findRightElf(new boolean[] {true, false, true, false, true}, 4)).isEqualTo(0);
-    assertThat(Problem19.findRightElf(new boolean[] {false, false, true, false, true}, 2)).isEqualTo(4);
+    assertThat(Problem19.findRightElf(new boolean[] {true, true, true, true, true}, 0, 5)).isEqualTo(1);
+    assertThat(Problem19.findRightElf(new boolean[] {true, false, true, true, true}, 2, 4)).isEqualTo(3);
+    assertThat(Problem19.findRightElf(new boolean[] {true, false, true, false, true}, 4, 3)).isEqualTo(0);
+    assertThat(Problem19.findRightElf(new boolean[] {false, false, true, false, true}, 2, 2)).isEqualTo(4);
+    assertThat(Problem19.findRightElf(new boolean[] {false, false, true, false, false}, 2, 1)).isEqualTo(2);
+  }
+
+  @Test
+  public void fiveElvesStealAcrossTheCircle() {
+    assertThat(Problem19.elfWithPresents(5, Problem19::acrossCircleElf)).isEqualTo(2);
+  }
+
+  @Test
+  public void acrossCircleElf() {
+    assertThat(Problem19.acrossCircleElf(new boolean[] {true, true, true, true, true}, 0, 5)).isEqualTo(2);
+    assertThat(Problem19.acrossCircleElf(new boolean[] {true, true, false, true, true}, 1, 4)).isEqualTo(4);
+    assertThat(Problem19.acrossCircleElf(new boolean[] {true, true, false, true, false}, 3, 3)).isEqualTo(0);
+    assertThat(Problem19.acrossCircleElf(new boolean[] {false, true, false, true, false}, 1, 2)).isEqualTo(3);
+    assertThat(Problem19.acrossCircleElf(new boolean[] {false, true, false, false, false}, 1, 1)).isEqualTo(1);
+  }
+
+  @Test
+  public void fiveElvesStealAcrossTheCircleEquation() {
+    assertThat(Problem19.stealAcrossRemaining(5)).isEqualTo(2);
+    assertThat(Problem19.stealAcrossRemaining(56)).isEqualTo(31);
+    assertThat(Problem19.stealAcrossRemaining(81)).isEqualTo(81);
+    assertThat(Problem19.stealAcrossRemaining(82)).isEqualTo(1);
+    assertThat(Problem19.stealAcrossRemaining(199)).isEqualTo(155);
   }
 }
