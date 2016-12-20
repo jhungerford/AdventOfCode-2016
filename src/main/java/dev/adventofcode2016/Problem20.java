6511c5d9ad66ff21a9dd707afcb07bab304b11b2
John Hungerford <jhungerford@gmail.com>
2016-12-20 08:32:01 -0700

    Problem 20 part 2.

--- src/main/java/dev/adventofcode2016/Problem20.java
+++ src/main/java/dev/adventofcode2016/Problem20.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.Comparator;
 import java.util.Deque;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class Problem20 {
@@ -27,18 +26,39 @@ public class Problem20 {
       String[] parts = range.split("-");
       return new Range(Long.parseLong(parts[0]), Long.parseLong(parts[1]));
     }
+
+    @Override
+    public String toString() {
+      return start + "-" + end;
+    }
   }
 
   /**
    * Finds the smallest value between 0 and max that doesn't fall into one of the ranges.
    *
-   * @param max Maximum allowed value
+   * @param max    Maximum allowed value
    * @param ranges Blacklisted ranges of values
    * @return Smallest allowed value
    */
   public static long smallestAllowed(long max, ImmutableList<Range> ranges) {
     // Collapsed ranges are sorted - one past the first range's end value is the first allowed value.
     return collapseRanges(ranges).get(0).end + 1;
+  }
+
+  /**
+   * Calculates the number of values between 0 and max (inclusive) that are don't fall into one of the given ranges.
+   *
+   * @param max    Maximum allowed value
+   * @param ranges Blacklisted range of values
+   * @return Number of values that are allowed
+   */
+  public static long numAllowed(long max, ImmutableList<Range> ranges) {
+    ImmutableList<Range> collapsed = collapseRanges(ranges);
+    long numBlacklisted = collapsed.stream()
+        .mapToLong(range -> range.end - range.start + 1)
+        .sum();
+
+    return max + 1 - numBlacklisted;
   }
 
   /**
@@ -65,9 +85,13 @@ public class Problem20 {
       Range second = sorted.removeFirst();
 
       if (second.start <= first.end + 1) {
-        sorted.addFirst(new Range(first.start, second.end));
+        sorted.addFirst(new Range(
+            Math.min(first.start, second.start),
+            Math.max(first.end, second.end)
+        ));
       } else {
         combined.add(first);
+        sorted.addFirst(second);
       }
     }
 
@@ -80,6 +104,9 @@ public class Problem20 {
         .map(Range::fromString)
         .collect(new ImmutableListCollector<>());
 
-    System.out.println("Part 1: smallest allowed value is " + Problem20.smallestAllowed(4294967295L, ranges));
+    long max = 4294967295L;
+
+    System.out.println("Part 1: smallest allowed value is " + Problem20.smallestAllowed(max, ranges));
+    System.out.println("Part 2: total allowed is " + Problem20.numAllowed(max, ranges));
   }
 }
--- src/test/java/dev/adventofcode2016/Problem20Test.java
+++ src/test/java/dev/adventofcode2016/Problem20Test.java
@@ -7,15 +7,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Problem20Test {
 
+  private static final ImmutableList<Problem20.Range> EXAMPLE_RANGES = ImmutableList.of(
+      new Problem20.Range(5, 8),
+      new Problem20.Range(0, 2),
+      new Problem20.Range(4, 7)
+  );
+
   @Test
   public void exampleSmallestAllowed() {
-    long smallest = Problem20.smallestAllowed(9, ImmutableList.of(
-        new Problem20.Range(5, 8),
-        new Problem20.Range(0, 2),
-        new Problem20.Range(4, 7)
-        )
-    );
+    assertThat(Problem20.smallestAllowed(9, EXAMPLE_RANGES)).isEqualTo(3);
+  }
 
-    assertThat(smallest).isEqualTo(3);
+  @Test
+  public void exampleTotalAllowed() {
+    assertThat(Problem20.numAllowed(9, EXAMPLE_RANGES)).isEqualTo(2);
   }
 }
