25ec9831ecfc8a4e35aebc74439fc23e01ca2755
John Hungerford <john.hungerford@cloudability.com>
2016-12-31 09:18:12 -0500

    Slow problem 22 solution.

--- src/main/java/dev/adventofcode2016/Problem22.java
+++ src/main/java/dev/adventofcode2016/Problem22.java
@@ -1,43 +1,86 @@
 package dev.adventofcode2016;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.Resources;
-import dev.adventofcode2016.util.ImmutableListCollector;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
-public class Problem22 {
+import dev.adventofcode2016.algorithms.AStar;
+import dev.adventofcode2016.util.ImmutableListCollector;
 
+public class Problem22 implements AStar<Problem22.Grid> {
 
-
-  public static class Node {
-    public final String filesystem;
+  public static class Position {
     public final int x;
     public final int y;
+
+    public Position(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof Position)) return false;
+      Position position = (Position) o;
+      return x == position.x &&
+          y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+      return "(" + x + ", " + y + ')';
+    }
+  }
+
+  public static class Node {
     public final int size;
     public final int used;
     public final int available;
     public final int usePercent;
 
-    public Node(String filesystem, int x, int y, int size, int used, int available, int usePercent) {
-      this.filesystem = filesystem;
-      this.x = x;
-      this.y = y;
+    public Node(int size, int used, int available, int usePercent) {
       this.size = size;
       this.used = used;
       this.available = available;
       this.usePercent = usePercent;
     }
 
+    /**
+     * Returns a copy of this node with all of the data gone.
+     * @return Empty copy of this node.
+     */
+    public Node empty() {
+      return new Node(size, 0, size, 0);
+    }
+
+    /**
+     * Returns a copy of this node with data copied from the given node.
+     * @param from Node to copy data from
+     * @return Copy of this node containing data from the given node
+     */
+    public Node withData(Node from) {
+      return new Node(size, from.used, size - from.used, from.used * 100 / size);
+    }
+
     @Override
     public String toString() {
-      return filesystem +
-          ", size=" + size + 'T' +
+      return "size=" + size + 'T' +
           ", used=" + used + 'T' +
           ", available=" + available + 'T' +
           ", usePercent=" + usePercent + '%';
@@ -48,22 +91,19 @@ public class Problem22 {
       if (this == o) return true;
       if (o == null || getClass() != o.getClass()) return false;
       Node node = (Node) o;
-      return x == node.x &&
-          y == node.y &&
-          size == node.size &&
+      return size == node.size &&
           used == node.used &&
           available == node.available &&
-          usePercent == node.usePercent &&
-          Objects.equals(filesystem, node.filesystem);
+          usePercent == node.usePercent;
     }
 
     @Override
     public int hashCode() {
-      return Objects.hash(filesystem, x, y, size, used, available, usePercent);
+      return Objects.hash(size, used, available, usePercent);
     }
 
     // "/dev/grid/node-x0-y0     94T   72T    22T   76%
-    private static final Pattern NODE_PATTERN = Pattern.compile("(/dev/grid/node-x(\\d+)-y(\\d+))\\s+(\\d+)T\\s+(\\d+)T\\s+(\\d+)T\\s+(\\d+)%");
+    private static final Pattern NODE_PATTERN = Pattern.compile("/dev/grid/node-x(\\d+)-y(\\d+)\\s+(\\d+)T\\s+(\\d+)T\\s+(\\d+)T\\s+(\\d+)%");
 
     public static Optional<Node> fromLine(String line) {
       Matcher matcher = NODE_PATTERN.matcher(line);
@@ -73,15 +113,196 @@ public class Problem22 {
       }
 
       return Optional.of(new Node(
-          matcher.group(1),
-          Integer.parseInt(matcher.group(2)),
           Integer.parseInt(matcher.group(3)),
           Integer.parseInt(matcher.group(4)),
           Integer.parseInt(matcher.group(5)),
-          Integer.parseInt(matcher.group(6)),
-          Integer.parseInt(matcher.group(7))
+          Integer.parseInt(matcher.group(6))
       ));
     }
+  }
+
+  public static class Grid {
+    public static final Grid GOAL = new Grid(null, null, new Position(0, 0));
+
+    public final Node[][] nodes;
+    public final Position empty;
+    public final Position goal;
+
+    public Grid(Node[][] nodes, Position empty, Position goal) {
+      this.nodes = nodes;
+      this.empty = empty;
+      this.goal = goal;
+    }
+
+    /**
+     * Returns an optional grid with the empty node moved to another location.
+     * The result will be present if the move is valid, and empty otherwise.
+     *
+     * @param position position to move the empty square to
+     * @return Grid with data moved if the move is valid, empty otherwise
+     */
+    public Optional<Grid> moveEmpty(Position position) {
+      if (position.x < 0 || position.x >= nodes[0].length || position.y < 0 || position.y >= nodes[0].length) {
+        return Optional.empty(); // Position is out of bounds.
+      }
+
+      Node from = nodes[empty.y][empty.x];
+      Node to = nodes[position.y][position.x];
+
+      if (to.available < from.used) {
+        return Optional.empty();
+      }
+
+      Node[][] newNodes = new Node[nodes[0].length][nodes.length];
+      for (int i = 0; i < nodes.length; i ++) {
+        System.arraycopy(nodes[i], 0, newNodes[i], 0, nodes[i].length);
+      }
+
+      newNodes[empty.y][empty.x] = newNodes[empty.y][empty.x].withData(newNodes[position.y][position.x]);
+      newNodes[position.y][position.x] = newNodes[position.y][position.x].empty();
+
+      Position newGoal = position.equals(goal) ? empty : goal;
+
+      return Optional.of(new Grid(newNodes, position, newGoal));
+    }
+
+    /**
+     * Renders the nodes for debugging purposes.  Empty nodes are marked as '_', the target is marked as G, and
+     * other nodes are marked as '.'.
+     *
+     * @return Strings that can be passed to System.out.println for rendering
+     */
+    public ImmutableList<String> render() {
+      ImmutableList.Builder<String> lines = ImmutableList.builder();
+
+
+      for (int y = 0; y < nodes.length; y ++) {
+        StringBuilder line = new StringBuilder();
+
+        for (int x = 0; x < nodes[y].length; x ++) {
+          Node node = nodes[y][x];
+
+          char nodeChar;
+          if (x == goal.x && y == goal.y) {
+            nodeChar = 'G';
+          } else if (node.usePercent == 0) {
+            nodeChar = '_';
+          } else {
+            nodeChar = '.';
+          }
+
+          line.append(nodeChar).append(' ');
+        }
+
+        lines.add(line.toString());
+      }
+
+      return lines.build();
+    }
+
+    /**
+     * Constructs a new grid from the given list of lines
+     *
+     * @param lines Lines describing the nodes
+     * @return Grid that represents the lines
+     */
+    public static Grid fromLines(Collection<String> lines) {
+      ImmutableList<Node> nodeList = lines.stream()
+          .map(Node::fromLine)
+          .filter(Optional::isPresent)
+          .map(Optional::get)
+          .collect(new ImmutableListCollector<>());
+
+      int sideLength = (int) Math.sqrt(nodeList.size());
+
+      Node[][] nodes = new Node[sideLength][sideLength];
+
+      Position empty = null;
+
+      // Nodes are ordered by x0-y0, x0-y1, ... in the lines
+      int index = 0;
+      for (int x = 0; x < sideLength; x ++) {
+        for (int y = 0; y < sideLength; y ++) {
+          nodes[y][x] = nodeList.get(index++);
+
+          if (nodes[y][x].usePercent == 0) {
+            empty = new Position(x, y);
+          }
+        }
+      }
+
+      Position goal = new Position(sideLength - 1, 0); // Goal is in the top right corner
+
+      return new Grid(nodes, empty, goal);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+      if (this == other) {
+        return true;
+      }
+
+      if (!(other instanceof Grid)) {
+        return false;
+      }
+
+      Grid grid = (Grid) other;
+      // Goal is at the end position - empty node position doesn't matter.
+      if (goal.equals(new Position(0, 0)) && goal.equals(grid.goal)) {
+        return true;
+      }
+
+      return goal.equals(grid.goal)
+          && empty.equals(grid.empty);
+//          && Arrays.deepEquals(nodes, grid.nodes);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(goal, empty, Arrays.deepHashCode(nodes));
+    }
+
+    @Override
+    public String toString() {
+      return Joiner.on('\n').join(render());
+    }
+  }
+
+  /**
+   * Calculates the taxicab distance between the goal in from and to.
+   *
+   * @param from Starting position
+   * @param to   Ending position
+   * @return Heuristic cost of moving the goal from 'from' to 'to'
+   */
+  @Override
+  public int heuristicCost(Grid from, Grid to) {
+    // Taxicab distance from the goal to the end
+    return Math.abs(from.goal.x - to.goal.x)
+        + Math.abs(from.goal.y - to.goal.y)
+        // Weight grids where the empty square is close to the goal more favorably
+        + (int) Math.sqrt(
+            Math.pow(Math.abs(from.empty.x - from.goal.x), 2)
+                + Math.pow(Math.abs(from.empty.y - from.goal.y), 2));
+  }
+
+  /**
+   * Returns the list of neighbors of the given grid.  The empty space will be moved in all four directions.
+   *
+   * @param grid Grid to check
+   * @return Neighbors of the grid
+   */
+  @Override
+  public ImmutableList<Grid> neighbors(Grid grid) {
+    return Stream.of(
+        grid.moveEmpty(new Position(grid.empty.x - 1, grid.empty.y)),
+        grid.moveEmpty(new Position(grid.empty.x + 1, grid.empty.y)),
+        grid.moveEmpty(new Position(grid.empty.x, grid.empty.y - 1)),
+        grid.moveEmpty(new Position(grid.empty.x, grid.empty.y + 1))
+    )
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .collect(new ImmutableListCollector<>());
   }
 
   /**
@@ -96,10 +317,9 @@ public class Problem22 {
    * @return Whether the transfer is viable
    */
   public static boolean isViable(Node from, Node to) {
-    return from.used != 0                      // From node isn't empty
-        && !(from.x == to.x && from.y == to.y) // Not the same node
-        && from.used <= to.available;          // Space for the transfer to complete
-
+    return from.used != 0             // From node isn't empty
+        && !(from.equals(to))         // Not the same node
+        && from.used <= to.available; // Space for the transfer to complete
   }
 
   public static void main(String[] args) throws IOException {
@@ -113,7 +333,9 @@ public class Problem22 {
         .mapToLong(fromNode -> nodes.stream().filter(toNode -> isViable(fromNode, toNode)).count())
         .sum();
 
-    System.out.println("Part 1: " + numViable + " viable pairs of nodes.");
+    Grid grid = Grid.fromLines(Resources.readLines(Resources.getResource("problem22.txt"), Charsets.UTF_8));
 
+    System.out.println("Part 1: " + numViable + " viable pairs of nodes.");
+    System.out.println("Part 2: fewest number of steps: " + (new Problem22().shortestPath(grid, Grid.GOAL).size() - 1));
   }
 }
--- src/test/java/dev/adventofcode2016/Problem22Test.java
+++ src/test/java/dev/adventofcode2016/Problem22Test.java
@@ -1,19 +1,98 @@
 package dev.adventofcode2016;
 
-import org.junit.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.collect.ImmutableList;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
 public class Problem22Test {
+
+  private static final ImmutableList<String> EXAMPLE_LINES = ImmutableList.of(
+      "Filesystem            Size  Used  Avail  Use%",
+      "/dev/grid/node-x0-y0   10T    8T     2T   80%",
+      "/dev/grid/node-x0-y1   11T    6T     5T   54%",
+      "/dev/grid/node-x0-y2   32T   28T     4T   87%",
+      "/dev/grid/node-x1-y0    9T    7T     2T   77%",
+      "/dev/grid/node-x1-y1    8T    0T     8T    0%",
+      "/dev/grid/node-x1-y2   11T    7T     4T   63%",
+      "/dev/grid/node-x2-y0   10T    6T     4T   60%",
+      "/dev/grid/node-x2-y1    9T    8T     1T   88%",
+      "/dev/grid/node-x2-y2    9T    6T     3T   66%"
+  );
+
+  private static final Problem22.Grid EXAMPLE = Problem22.Grid.fromLines(EXAMPLE_LINES);
+
 
   @Test
   public void parseNode() {
     assertThat(Problem22.Node.fromLine("/dev/grid/node-x0-y0     94T   72T    22T   76%"))
-        .hasValue(new Problem22.Node("/dev/grid/node-x0-y0", 0, 0, 94, 72, 22, 76));
+        .hasValue(new Problem22.Node( 94, 72, 22, 76));
   }
 
   @Test
   public void parseNonNode() {
     assertThat(Problem22.Node.fromLine("root@ebhq-gridcenter# df -h")).isEmpty();
   }
+
+  @Test
+  public void visualizeExample() {
+    assertThat(EXAMPLE.render()).containsExactly(
+        ". . G ",
+        ". _ . ",
+        ". . . "
+    );
+  }
+
+  @Test
+  public void fewestStepsExample() {
+    assertThat(new Problem22().shortestPath(EXAMPLE, Problem22.Grid.GOAL)).hasSize(8);
+  }
+
+  @Test
+  public void exampleMoveDown() {
+    Optional<Problem22.Grid> moved = EXAMPLE.moveEmpty(new Problem22.Position(1, 2));
+    assertThat(moved.isPresent());
+
+    Problem22.Grid actual = moved.get();
+    Problem22.Grid expected = new Problem22.Grid(new Problem22.Node[][]{
+        new Problem22.Node[]{EXAMPLE.nodes[0][0], EXAMPLE.nodes[0][1], EXAMPLE.nodes[0][2]},
+        new Problem22.Node[]{EXAMPLE.nodes[1][0], EXAMPLE.nodes[1][1].withData(EXAMPLE.nodes[2][1]), EXAMPLE.nodes[1][2]},
+        new Problem22.Node[]{EXAMPLE.nodes[2][0], EXAMPLE.nodes[2][1].empty(), EXAMPLE.nodes[2][2]}
+    },
+        new Problem22.Position(1, 2),
+        new Problem22.Position(2, 0)
+    );
+
+    assertThat(actual.nodes).isEqualTo(expected.nodes);
+    assertThat(actual.empty).isEqualTo(expected.empty);
+    assertThat(actual.goal).isEqualTo(expected.goal);
+  }
+
+  @Test
+  public void exampleMoveGoal() {
+    // Move the empty square up and right
+    Optional<Problem22.Grid> up = EXAMPLE.moveEmpty(new Problem22.Position(1, 0));
+    assertThat(up.isPresent());
+
+    Optional<Problem22.Grid> right = up.get().moveEmpty(new Problem22.Position(2, 0));
+    assertThat(right.isPresent());
+
+    Problem22.Grid actual = right.get();
+    Problem22.Grid expected = new Problem22.Grid(new Problem22.Node[][]{
+        new Problem22.Node[]{EXAMPLE.nodes[0][0], EXAMPLE.nodes[0][1].withData(EXAMPLE.nodes[0][2]), EXAMPLE.nodes[0][2].empty()},
+        new Problem22.Node[]{EXAMPLE.nodes[1][0], EXAMPLE.nodes[1][1].withData(EXAMPLE.nodes[0][1]), EXAMPLE.nodes[1][2]},
+        new Problem22.Node[]{EXAMPLE.nodes[2][0], EXAMPLE.nodes[2][1], EXAMPLE.nodes[2][2]}
+    },
+        new Problem22.Position(2, 0),
+        new Problem22.Position(1, 0)
+    );
+
+    assertThat(actual.nodes).isEqualTo(expected.nodes);
+    assertThat(actual.empty).isEqualTo(expected.empty);
+    assertThat(actual.goal).isEqualTo(expected.goal);
+  }
+
 }
